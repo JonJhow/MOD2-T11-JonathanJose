511d1802b758d98df6aa9ed2271b4043941c3646
JonJhow <jonathan.marques2023@gmail.com>
2022-11-24 16:29:54 -0300

    Correções Bird

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -8,21 +8,23 @@ class Bird(Obstacle):
     def __init__(self):
         self.image = BIRD
         self.type = 0
+        self.fly_index = 0    
         self.fly_position = random.randint(1, 3)
-        self.fly_index = 0
-        super().__init__(self.image, self.type)
+        super().__init__(self.image, self.type)   
 
+    def draw(self, screen):
+        self.type = 0 if self.fly_index > 5 else 1
+        
         if self.fly_position == 1:
             self.rect.y = 340 if self.type == 0 else 326
         elif self.fly_position == 2:
             self.rect.y = 270 if self.type == 0 else 256
         elif self.fly_position == 3:
             self.rect.y = 230 if self.type == 0 else 216
+        
+        screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
 
-    def draw(self,screen):
-        x = self.fly_index // 5
-        screen.blit(self.image[x], self.rect)
         self.fly_index += 1
-
-        if self.fly_index >= 9:
+        
+        if self.fly_index > 10:
             self.fly_index = 0
