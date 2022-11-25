4b381414516bbf438f82a1bc2fd52c3c9fb6056c
JonJhow <jonathan.marques2023@gmail.com>
2022-11-24 23:14:16 -0300

    AULA 4: Power up manager, Shield

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,9 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 FONT_STYLE = "freesansbold.ttf"
 
@@ -18,12 +19,13 @@ class Game:
         self.running = False
         self.game_speed = 20
         self.score = 0
-        self.high_score = 0
+        self.higher_score = 0
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.death_count = 0
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
 
     def execute(self):
         self.running = True
@@ -38,14 +40,15 @@ class Game:
         # Game loop: events - update - draw
         self.playing = True
         self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups()
         self.score = 0
         self.game_speed = 20
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > self.higher_score:
+            self.higher_score = self.score
 
     def events(self):
         for event in pygame.event.get():
@@ -58,6 +61,8 @@ class Game:
         self.player.update(user_input)
         self.obstacle_manager.update(self)
         self.update_score()
+        self.power_up_manager.update(self.score, self.game_speed, self.player)
+
 
     def update_score(self):
         self.score += 1
@@ -71,6 +76,8 @@ class Game:
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         self.draw_score()
+        self.draw_power_up_time()
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -83,7 +90,7 @@ class Game:
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
 
-    def text_layout(self, text, size, x_pos, y_pos):
+    def text_draw(self, text, size, x_pos, y_pos):
         font = pygame.font.Font(FONT_STYLE, size)
         text = font.render(text, True, (0, 0, 0))
         text_rect = text.get_rect()
@@ -91,7 +98,17 @@ class Game:
         self.screen.blit(text, text_rect)
 
     def draw_score(self):
-        self.text_layout(f"Score {self.score}", 22, 1000, 50)
+        self.text_draw(f"Score {self.score}", 22, 1000, 50)
+
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks()) / 1000, 2)
+            if time_to_show >= 0:
+                self.text_draw(
+                    f'{self.player.type.capitalize()} enabled for {time_to_show} seconds', 22, 500, 40)
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
 
     def handle_events_on_menu(self):
         for event in pygame.event.get():
@@ -107,13 +124,13 @@ class Game:
         half_screen_width = SCREEN_WIDTH // 2
 
         if self.death_count == 0:
-            self.text_layout("Press any key to start!", 30, half_screen_width, half_screen_height)
+            self.text_draw("Press any key to start!", 30, half_screen_width, half_screen_height)
         else:
             self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 140))
-            self.text_layout(f"Your score: {self.score}", 22, half_screen_width, half_screen_height)
-            self.text_layout(f"Your high score: {self.high_score}", 22,  half_screen_width, half_screen_height + 22)
-            self.text_layout(f'Deaths counter: {self.death_count}', 22, half_screen_width, half_screen_height + 44)
-            self.text_layout("Press any key to restart!", 30, half_screen_width, half_screen_height + 130)
+            self.text_draw(f"Your score: {self.score}", 22, half_screen_width, half_screen_height)
+            self.text_draw(f"Your higher score: {self.higher_score}", 22,  half_screen_width, half_screen_height + 22)
+            self.text_draw(f'Deaths counter: {self.death_count}', 22, half_screen_width, half_screen_height + 44)
+            self.text_draw("Press any key to restart!", 30, half_screen_width, half_screen_height + 130)
 
         pygame.display.update()
         self.handle_events_on_menu()
